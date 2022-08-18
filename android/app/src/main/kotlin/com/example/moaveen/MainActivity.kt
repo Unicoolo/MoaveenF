974b8cf481f6ff4238981f08f5697f864b894ef3
Unicoolo <f2018266170@gmail.com>
2022-08-18 13:12:47 +0500

    login_singup

---
+++ android/app/src/main/kotlin/com/example/moaveen/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.moaveen
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Services/global_variables.dart
@@ -0,0 +1,3 @@
+
+String loginUrlImage='https://myolyn.com/wp-content/uploads/2021/05/5AF4BA80-D32F-49D0-B527-64E3956A67CF_1_105_c-e1622226269391.jpeg';
+String signUpUrlImage='https://i0.wp.com/disabilityvisibilityproject.com/wp-content/uploads/2020/06/dah-party03.jpg?resize=1024%2C722&ssl=1';
